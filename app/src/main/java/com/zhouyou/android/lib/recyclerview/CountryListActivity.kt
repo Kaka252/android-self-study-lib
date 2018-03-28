fb7a75ceac1f0253f430129b2d80b91e79d8922c
zhouyou <kaka252@sina.com>
2018-03-28 22:09:22 +0800

    城市列表

--- app/src/main/java/com/zhouyou/android/lib/recyclerview/CountryListActivity.kt
+++ app/src/main/java/com/zhouyou/android/lib/recyclerview/CountryListActivity.kt
@@ -1,19 +1,24 @@
 package com.zhouyou.android.lib.recyclerview
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import com.zhouyou.android.lib.R
 import kotlinx.android.synthetic.main.activity_country_list.*
+import java.io.Serializable
 
 class CountryListActivity : AppCompatActivity() {
+
+    private val DATA_ENTITY = "COUNTRY_DATA"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_country_list)
 
-        val list = getData()
-        refreshAdapter(list as ArrayList<LevelBean>)
+        val t = Thread(runnable)
+        t.start()
     }
 
     private fun getData() : List<LevelBean> {
@@ -23,10 +28,27 @@ class CountryListActivity : AppCompatActivity() {
     }
 
     private fun refreshAdapter(list : ArrayList<LevelBean>) {
-
         val manager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         rv_list.layoutManager = manager
         val adapter = CountryListAdapter(this, list)
         rv_list.adapter = adapter
     }
+
+    private val runnable = Runnable {
+        val countryList = getData()
+        val data = Bundle()
+        data.putSerializable(DATA_ENTITY, countryList as Serializable)
+        val message = Message.obtain()
+        message.data = data
+        handlerCall.sendMessage(message)
+    }
+
+    private val handlerCall = Handler(Handler.Callback { msg ->
+        val data = msg.data
+        if (data != null) {
+            val countryList = data.getSerializable(DATA_ENTITY) as ArrayList<LevelBean>
+            refreshAdapter(countryList)
+        }
+        true
+    })
 }

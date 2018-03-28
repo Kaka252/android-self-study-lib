d1c8653c1222077930581d196484783f52bef0e1
zhouyou <kaka252@sina.com>
2018-03-28 21:55:16 +0800

    城市列表

--- app/src/main/java/com/zhouyou/android/lib/recyclerview/CountryListActivity.kt
+++ app/src/main/java/com/zhouyou/android/lib/recyclerview/CountryListActivity.kt
@@ -2,6 +2,7 @@ package com.zhouyou.android.lib.recyclerview
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import com.zhouyou.android.lib.R
 import kotlinx.android.synthetic.main.activity_country_list.*
 
@@ -11,23 +12,20 @@ class CountryListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_country_list)
 
-
-
+        val list = getData()
+        refreshAdapter(list as ArrayList<LevelBean>)
     }
 
-    fun getData() : List<LevelBean> {
-        val list : List<LevelBean> = ArrayList()
-        for (i : Int in 0..10) {
-//            var item = LevelBean(i, "")
-//            list. = item
-        }
-
-
-
+    private fun getData() : List<LevelBean> {
+        val list : ArrayList<LevelBean> = ArrayList()
+        (0..10).mapTo(list) { LevelBean(it.toLong(), "城市") }
         return list
     }
 
-    fun refreshAdapter(list : List<LevelBean>) {
+    private fun refreshAdapter(list : ArrayList<LevelBean>) {
+
+        val manager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        rv_list.layoutManager = manager
         val adapter = CountryListAdapter(this, list)
         rv_list.adapter = adapter
     }
--- app/src/main/java/com/zhouyou/android/lib/recyclerview/CountryListAdapter.kt
+++ app/src/main/java/com/zhouyou/android/lib/recyclerview/CountryListAdapter.kt
@@ -10,15 +10,10 @@ import com.zhouyou.android.lib.R
  * Author: ZhouYou
  * Date: 2018/3/28.
  */
-class CountryListAdapter : RecyclerView.Adapter<CountryItemViewHolder> {
+class CountryListAdapter(activity: Activity, list: List<LevelBean>) : RecyclerView.Adapter<CountryItemViewHolder>() {
 
-    private var activity: Activity? = null
-    private var list: List<LevelBean>? = null
-
-    constructor(activity: Activity, list: List<LevelBean>) : super() {
-        this.activity = activity
-        this.list = list
-    }
+    private var activity: Activity? = activity
+    private var list: List<LevelBean>? = list
 
     override fun onBindViewHolder(holder: CountryItemViewHolder, position: Int) {
         val bean = getItem(position)

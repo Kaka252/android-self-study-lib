4017182c65b4931cb15f574cf2ba94e4618681e8
zhouyou <kaka252@sina.com>
2018-04-08 16:11:56 +0800

    gradle task study

--- common-plugin/src/main/groovy/common/plugin/UserPlugin.groovy
+++ common-plugin/src/main/groovy/common/plugin/UserPlugin.groovy
@@ -3,23 +3,30 @@ package common.plugin
 import org.gradle.api.Action
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.Task
 
 class UserPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-        println '===This is a demo of get user info task==='
-        project.getTasks().create("get-user-info-task", GetUserInfo.class, new Action<GetUserInfo>() {
+        project.tasks.create("get-user-info-task", GetUserInfo.class, new Action<GetUserInfo>() {
 
             @Override
             void execute(GetUserInfo info) {
                 info.setUserAge('28岁')
                 info.setUserName('郭峰')
             }
+        }).doFirst(new Action<Task>() {
+            @Override
+            void execute(Task task) {
+                println '===This is a demo of get user info task==='
+            }
+        }).doLast(new Action<Task>() {
+            @Override
+            void execute(Task task) {
+                println 'This is a get user info task created by zhouyou.'
+            }
         })
-        println 'This is a plugin for common user.'
-        project.task('get-user-info-task').doFirst {
-            println 'This is a get user info task created by zhouyou.'
-        }
+
     }
 }
